c0f10c554998c2ef61c082dd99ae43a1bf955328
Svelez12 <velez.steven12@gmail.com>
2020-04-18 18:42:57 -0400

    Implement InstanceLocator & MainViewModel

---
+++ TicTacToe/TicTacToe/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TicTacToe.ViewModels;
+
+namespace TicTacToe.Infrastructure
+{
+   public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TicTacToe.ViewModels
+{
+    public class MainViewModel
+    {
+        private static MainViewModel instance;
+
+        public MainViewModel()
+        {
+            instance = this;
+        }
+
+    }
+}
